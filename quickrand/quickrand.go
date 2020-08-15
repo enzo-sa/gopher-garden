1c551d1871ae282fe93e284a1f9318110ca57b90
enzo <enzostack@gmail.com>
2020-08-14 22:30:40 -0700

    lint

--- quickrand/quickrand.go
+++ quickrand/quickrand.go
@@ -8,7 +8,7 @@ import (
 
 // package to make some of the rng requirements for the game easier, e.g distinct mode
 
-// gets n random int values between 0 and max range efficiently w/ distinct option
+// RandInts gets n random int values between 0 and max range efficiently w/ distinct option
 func RandInts(max, n int, distinct bool) *[]int {
 	vals := make([]int, n)
 	if distinct {
@@ -21,14 +21,14 @@ func RandInts(max, n int, distinct bool) *[]int {
 		panic("Non positive argument to RandIntRange or distinct is impossibly true.")
 	}
 	src := rand.NewSource(time.Now().UnixNano())
-	max_bitrep := bitrep(max)
-	val_per_rand := 63 / max_bitrep // 63 / max_bitrep == num of possible value fills per int63 call
-	var mask int64 = (1 << max_bitrep) - 1
+	maxBitrep := bitrep(max)
+	valPerRand := 63 / maxBitrep // 63 / maxBitrep == num of possible value fills per int63 call
+	var mask int64 = (1 << maxBitrep) - 1
 	// can improve by recycling leftover bits that couldnt represent full value, but probably not worth it
-	for bits, rem, i := src.Int63(), val_per_rand, 0; i < n; {
+	for bits, rem, i := src.Int63(), valPerRand, 0; i < n; {
 		if rem == 0 {
 			bits = src.Int63() // refill random bits and reset rem
-			rem = val_per_rand
+			rem = valPerRand
 		}
 		val := bits & mask
 		if val <= int64(max) && (!distinct || !InVals(int(val), &vals)) {
@@ -36,13 +36,12 @@ func RandInts(max, n int, distinct bool) *[]int {
 			i++
 		}
 		rem--
-		bits >>= max_bitrep
+		bits >>= maxBitrep
 	}
 	return &vals
 }
 
-// exported as it is useful for some other functions in
-// different packages
+// InVals linearly searches value set for value and returns true if found
 func InVals(val int, vals *[]int) bool {
 	for i := range *vals {
 		if val == (*vals)[i] {
@@ -52,7 +51,7 @@ func InVals(val int, vals *[]int) bool {
 	return false
 }
 
-// returns number of bits required to represent max
+// bitrep returns number of bits required to represent max
 func bitrep(max int) int {
 	size := unsafe.Sizeof(max)
 	var mask int = 1 << (size*8 - 2)
